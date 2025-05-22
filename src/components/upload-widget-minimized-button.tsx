0c111ee3d0165863ae3ce5d5a4e0f9dd8fc723d5
Mateus Paula <mateuspaula@Laptop-de-Mateus.local>
2025-05-21 23:16:09 -0300

    feat: add animations and a few states

--- src/components/upload-widget-minimized-button.tsx
+++ src/components/upload-widget-minimized-button.tsx
@@ -1,11 +1,12 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { Maximize2 } from 'lucide-react';
+import { UploadWidgetTitle } from './upload-widget-title';
 
 export function UploadWidgetMinimizedButton() {
     return (
-        <Collapsible.Trigger className='group w-full bg-white/2 py-3 px-5 flex items-center justify-between'>
-            <span className="text-sm font-medium">Upload files</span>
-            
+        <Collapsible.Trigger className='group w-full bg-white/2 py-3 px-5 flex items-center justify-between gap-5'>
+            <UploadWidgetTitle/>
+                        
 
             {/* StrikeWidth: controla a grossura das linhas */}
             <Maximize2 strokeWidth={1.5} className="size-4 text-zinc-400 group-hover:text-zinc-100"/>
